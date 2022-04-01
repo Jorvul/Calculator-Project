0d16d49f1f91ac91f6ee64a53f9150864e26abe4
Jorge Fernandez <jorgelfc1989@gmail.com>
2022-04-01 15:10:19 -0400

    first commit

---
+++ src/main/java/com/jorge/calculator/project/MainApp.java
@@ -0,0 +1,9 @@
+package com.jorge.calculator.project;
+
+public class MainApp {
+public static void main(String[] args) {
+    Calculator c=new Calculator();
+    
+    System.out.println("Adding Two Numbers : " +c.add(2, 2));
+}
+}
